17b445b2f75d96c1af1fc8349b4be1afdd1a226b
Srujan Gurram <srujangs8@gmail.com>
2024-03-24 18:27:51 +0530

    Refactor landing page layout to optimize responsiveness

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -3,17 +3,18 @@ import RequirementsForm from "./RequirementsForm";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen">
-			<div className="flex-1 grid place-items-center hero-pattern border-r border-pink-500">
+		<main className="flex flex-col md:flex-row min-h-screen">
+			<div className="md:flex-1 grid place-items-center hero-pattern border-r border-pink-500 p-10">
 				<Image
 					src="/iq-storm-large.png"
 					alt="A picture of brain with lightning bolt below it"
 					width={477}
 					height={477}
 					quality={100}
+					className="size-20 md:size-96 object-contain"
 				/>
 			</div>
-			<div className="flex-1 grid place-items-center">
+			<div className="md:flex-1 grid place-items-center p-10">
 				<RequirementsForm />
 			</div>
 		</main>
