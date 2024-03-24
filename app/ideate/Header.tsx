7fbb08fa7d324667598675055f41656c772fadc9
Srujan Gurram <srujangs8@gmail.com>
2024-03-24 19:28:43 +0530

    Update background and card colors in globals.css and increase size of image in Header.tsx

--- app/ideate/Header.tsx
+++ app/ideate/Header.tsx
@@ -9,14 +9,14 @@ export interface HeaderProps {
 
 function Header({ purpose, domains, technologies }: HeaderProps) {
 	return (
-		<header className="flex flex-col items-center justify-center">
+		<header className="flex flex-col gap-4 items-center justify-center">
 			<Image
 				src="/iq-storm.png"
 				alt="A picture of brain with lightning bolt below it"
-				width={80}
-				height={80}
+				width={128}
+				height={128}
 				quality={100}
-				className="size-20 object-contain"
+				className="size-32 object-contain"
 			/>
 			<p className="text-lg text-center max-w-2xl">
 				Generating ideas for{" "}
