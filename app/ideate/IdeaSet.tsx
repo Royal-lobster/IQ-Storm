176e35bb3096f7e65ff091ee5f82eaf1df2bc202
Srujan Gurram <srujangs8@gmail.com>
2024-03-25 17:48:08 +0530

    Update IdeaCard component to toggle between checked and on states based on user interaction

--- app/ideate/IdeaSet.tsx
+++ app/ideate/IdeaSet.tsx
@@ -1,29 +1,49 @@
 "use client";
 
 import React from "react";
-import * as RadioGroup from "@radix-ui/react-radio-group";
+import * as ToggleGroup from '@radix-ui/react-toggle-group';
 import IdeaCard from "./IdeaCard";
 import type { Idea } from "../api/ideate/schema";
 import LoadingIdeaCard from "./LoadingIdeaCard";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { PlusIcon, ReloadIcon } from "@radix-ui/react-icons";
 
 interface IdeaSetProps {
   ideas: Idea[];
   isActive?: boolean;
   isFetching?: boolean;
+  defaultCheckedIdea?: string;
   count: number;
 }
 
-function IdeaSet({ ideas, isActive, isFetching, count }: IdeaSetProps) {
+function IdeaSet({ ideas, isActive, isFetching, count, defaultCheckedIdea }: IdeaSetProps) {
+  const [checkedIdea, setCheckedIdea] = React.useState<string | undefined>(defaultCheckedIdea);
+
   return (
-    <RadioGroup.Root className="flex gap-4" disabled={!isActive || isFetching}>
-      {isFetching
-        ? Array.from({ length: count }).map((_) => <LoadingIdeaCard />)
-        : ideas.map((idea, i) => (
-          <RadioGroup.Item value={String(i + 1)} className="group">
-            <IdeaCard title={idea.title} description={idea.description} />
-          </RadioGroup.Item>
-        ))}
-    </RadioGroup.Root>
+    <div className={cn("border p-6 w-max rounded-md", isActive ? "bg-primary/10 border-primary" : "")}>
+      {isActive && <h2 className="text-xl font-semibold text-primary mb-4">Which one do you like?</h2>}
+      <ToggleGroup.Root className="flex justify-center items-stretch flex-wrap gap-4"
+        type="single"
+        disabled={!isActive || isFetching}
+        onValueChange={setCheckedIdea}
+        value={checkedIdea}
+      >
+        {isFetching
+          ? [...Array(count).keys()].map((i) => <LoadingIdeaCard key={i} />)
+          : ideas.map((idea, i) => (
+            <ToggleGroup.Item value={String(i + 1)} className="group">
+              <IdeaCard title={idea.title} description={idea.description} />
+            </ToggleGroup.Item>
+          ))}
+      </ToggleGroup.Root>
+      {isActive && (
+        <Button variant="outline" type="submit" className="translate-y-10 mx-auto flex border-primary">
+          {checkedIdea ? "Generate More" : "Don't like any"}
+          {checkedIdea ? <PlusIcon /> : <ReloadIcon />}
+        </Button>
+      )}
+    </div >
   );
 }
 
--- app/ideate/page.tsx
+++ app/ideate/page.tsx
@@ -1,5 +1,6 @@
 import { requirementsSchema } from "../api/ideate/schema";
 import Header from "./Header";
+import IdeaSet from "./IdeaSet";
 
 export interface IdeatePageProps {
 	searchParams: Record<string, string>;
@@ -11,6 +12,28 @@ function IdeatePage({ searchParams }: IdeatePageProps) {
 	return (
 		<div className="container py-20">
 			<Header purpose={purpose} domains={domains} technologies={technologies} />
+			<div className="space-y-10 mt-10 flex flex-col items-center justify-center">
+				<IdeaSet ideas={[{
+					title: "Crypto Education Platform with Al-Powered Learning Paths",
+					description: "A platform that uses AI to create personalized learning paths for users to learn about blockchain and cryptocurrencies.",
+				}, {
+					title: "Decentralized Healthcare Marketplace",
+					description: "A decentralized marketplace where patients can buy and sell healthcare services using cryptocurrencies.",
+				}]} isActive={false} count={2} isFetching={false} />
+
+				<IdeaSet
+					ideas={[{
+						title: "AI-Powered Crypto Trading Bot",
+						description: "A trading bot that uses AI to analyze market trends and make profitable trades.",
+					}, {
+						title: "Blockchain-Based Voting System",
+						description: "A secure and transparent voting system that uses blockchain technology to prevent fraud.",
+					}]}
+					isActive={true}
+					count={2}
+					isFetching={false}
+				/>
+			</div>
 		</div>
 	);
 }
