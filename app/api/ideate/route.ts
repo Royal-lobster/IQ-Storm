61e85e93574e84cb42a22e5c6f15f6144a5755d8
Yadheedhya <yadheedhya6@gmail.com>
2024-03-25 19:12:01 +0530

    clean code

--- app/api/ideate/route.ts
+++ app/api/ideate/route.ts
@@ -1,20 +1,10 @@
 import { ChatOpenAI } from "@langchain/openai";
 import { NextResponse, type NextRequest } from "next/server";
-import { ideateInputSchema, requirementsSchema } from "./schema";
-import {
-	generateIdeaFromTwoIdeasPromptTemplate,
-	generateInitialIdeasPromptTemplate,
-} from "@/lib/prompts";
-import {
-	NumberedListOutputParser,
-	StringOutputParser,
-} from "@langchain/core/output_parsers";
-import { RunnableSequence } from "@langchain/core/runnables";
+import { requirementsSchema } from "./schema";
+import {generateInitialIdeasPromptTemplate,} from "@/lib/prompts";
 import { JsonOutputFunctionsParser } from "langchain/output_parsers";
 import { z } from "zod";
 import { zodToJsonSchema } from "zod-to-json-schema";
-import { HumanMessage } from "@langchain/core/messages";
-
 
 /**
  * Handles the POST request for generating ideas. It takes in all
@@ -57,60 +47,7 @@ export const POST = async (req: NextRequest) => {
 
 	console.log("Got initial ideas", initialIdeas);
 
-	// A recursive function that generates intermediate ideas until only one idea is left.
-	// const run = async (
-	// 	previousNodes: string[],
-	// 	allIntermediateIdeas: string[][],
-	// ): Promise<string[]> => {
-	// 	const intermediateIdeas = await RunnableSequence.from([
-	// 		generateIdeaFromTwoIdeasPromptTemplate,
-	// 		model,
-	// 		new StringOutputParser(),
-	// 	]).batch(
-	// 		createIdeaGroupings(previousNodes).map(([idea1, idea2]) => ({
-	// 			purpose,
-	// 			domains: domains,
-	// 			additionalInformation,
-	// 			idea1,
-	// 			idea2,
-	// 			technologies,
-	// 		})),
-	// 	);
-	// 	console.log("Got intermediate ideas", intermediateIdeas);
-	// 	allIntermediateIdeas.push(intermediateIdeas);
-	// 	return intermediateIdeas.length === 1
-	// 		? intermediateIdeas
-	// 		: await run(intermediateIdeas, allIntermediateIdeas);
-	// };
-
-	// console.log("Running the recursive function");
-
-	// const allIntermediateIdeas: string[][] = [];
-	// const finalIdea = (await run(initialIdeas, allIntermediateIdeas))[0];
-
-	// console.log("Got final idea", finalIdea);
-
 	return NextResponse.json({
 		initialIdeas,
-		// allIntermediateIdeas,
-		// finalIdea,
 	});
-};
-
-/**
- * Creates pairs of ideas from an array of strings.
- *
- * @param ideas - An array of strings representing ideas.
- * @returns An array of pairs, where each pair consists of two consecutive ideas from the input array.
- */
-const createIdeaGroupings = (ideas: string[]) => {
-	const ideaGroupings: [string, string][] = [];
-
-	for (let i = 0; i < ideas.length; i++) {
-		if (i === 0) continue;
-		const pair: [string, string] = [ideas[i - 1], ideas[i]];
-		ideaGroupings.push(pair);
-	}
-
-	return ideaGroupings;
 };
