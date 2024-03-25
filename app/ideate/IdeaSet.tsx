ba9d25b49c88ef8f84ae77ac31609531137f519e
Srujan Gurram <srujangs8@gmail.com>
2024-03-25 17:57:30 +0530

    Add Label and Textarea components for user feedback input in IdeaSet component

--- app/ideate/IdeaSet.tsx
+++ app/ideate/IdeaSet.tsx
@@ -8,6 +8,8 @@ import LoadingIdeaCard from "./LoadingIdeaCard";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { PlusIcon, ReloadIcon } from "@radix-ui/react-icons";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
 interface IdeaSetProps {
   ideas: Idea[];
@@ -32,11 +34,20 @@ function IdeaSet({ ideas, isActive, isFetching, count, defaultCheckedIdea }: Ide
         {isFetching
           ? [...Array(count).keys()].map((i) => <LoadingIdeaCard key={i} />)
           : ideas.map((idea, i) => (
-            <ToggleGroup.Item value={String(i + 1)} className="group">
+            <ToggleGroup.Item value={String(i + 1)} className="group" key={idea.title}>
               <IdeaCard title={idea.title} description={idea.description} />
             </ToggleGroup.Item>
           ))}
       </ToggleGroup.Root>
+      {isActive && checkedIdea && (
+        <div className="grid w-full gap-1.5 mt-6">
+          <Label htmlFor="feedback">Why did you like it ?</Label>
+          <Textarea placeholder="Type your message here." id="feedback" />
+          <p className="text-sm text-muted-foreground">
+            This will help both you and me to know what you really want :)
+          </p>
+        </div>
+      )}
       {isActive && (
         <Button variant="outline" type="submit" className="translate-y-10 mx-auto flex border-primary">
           {checkedIdea ? "Generate More" : "Don't like any"}
