2bebe2392816b2d5bf96e7ce710048cf225ddb31
Srujan Gurram <srujangs8@gmail.com>
2024-03-27 15:01:11 +0530

    Add error handling for server errors in createSafeActionClient method

--- lib/safe-action.ts
+++ lib/safe-action.ts
@@ -1,3 +1,10 @@
-import { createSafeActionClient } from "next-safe-action";
+import { DEFAULT_SERVER_ERROR, createSafeActionClient } from "next-safe-action";
 
-export const action = createSafeActionClient();
+export const action = createSafeActionClient({
+	handleReturnedServerError(e) {
+		if (e instanceof Error) {
+			return e.message;
+		}
+		return DEFAULT_SERVER_ERROR;
+	},
+});
